d1ead6e5f8eeed754e7d3eb05818ac8bbc144ec8
Eduardo Moreira Viter <moreiraviter@outlook.com>
2024-09-28 22:24:58 -0300

    terceiro commit

--- src/java/dao/LivrosDAO.java
+++ src/java/dao/LivrosDAO.java
@@ -7,6 +7,8 @@ package dao;
 import apoio.ConexaoBD;
 import entidade.Livros;
 import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  *
@@ -35,9 +37,89 @@ public class LivrosDAO {
 
             return true;
         } catch (Exception e) {
-            System.out.println("Erro ao SALVAR LIVRO:" + e);
+            System.out.println("Erro ao SALVAR LIVROS:" + e);
             return false;
         }
     }
+    
+    //consultar todos
+    public ArrayList consultar (){
+        ArrayList<Livros> livros = new ArrayList<>();
+        
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
 
+            String sql = ""
+                    + "select * from livros";
+
+            System.out.println("SQL:" + sql);
+
+            ResultSet resultado =  st.executeQuery(sql);
+            
+            while(resultado.next()){
+                Livros l = new Livros();
+                l.setId(resultado.getInt("id"));
+                l.setTitulo(resultado.getString("titulo"));
+                l.setAutor(resultado.getString("autor"));
+                l.setDescricao(resultado.getString("descrição"));
+                l.setPublicacai(resultado.getString("publicacai"));
+                l.setAvaliacao(resultado.getString("avaliação"));
+                
+                livros.add(l);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao CONSULTAR LIVROS:" + e);
+            
+        }
+           
+        return livros;
+    } 
+    
+    public boolean excluir(int parametro) {
+
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = ""
+                    + "delete from livros "
+                    + "where "
+                    + "id = " + parametro;
+
+            System.out.println("SQL:" + sql);
+
+            st.executeUpdate(sql);
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("Erro ao EXCLUIR LIVROS:" + e);
+            return false;
+        }
+    }
+    
+    public boolean atualizar(Livros c) {
+
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = ""
+                    + "update livros "
+                    + "set "
+                    + "titulo = '" + c.getTitulo() + "', "
+                    + "autor = '" + c.getAutor() + "', "
+                    + "descrição = '" + c.getDescricao() + "',"
+                    + "publicacai = '" + c.getPublicacai() + "', "
+                    + "avaliação = '" + c.getAvaliacao() + "', "
+                    + "where id = '" + c.getId();
+
+            System.out.println("SQL:" + sql);
+
+            st.executeUpdate(sql);
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("Erro ao ATUALIZAR LIVROS:" + e);
+            return false;
+        }
+    }    
 }
